c21ef53860fc40d30c71ebc1d7f365858fbd44fd
roblesdotdev <httpsnip@gmail.com>
2024-08-22 16:12:36 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
