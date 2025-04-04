997e6e3c6b41c10b9bffcfcbcd33b17ef61a1f7b
roblesdotdev <httpsnip@gmail.com>
2025-04-04 14:20:56 -0300

    setup tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { render } from "preact";
-import "./index.css";
+import "./styles/fonts.css";
+import "./styles/global.css";
 import App from "./app.tsx";
 
 const root = document.getElementById("root");
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [preact()],
+  plugins: [tailwindcss(), preact()],
 });
